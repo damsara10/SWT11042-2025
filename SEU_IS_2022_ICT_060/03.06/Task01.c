af9f6fcfe5b33ef4bd497e7ff8f78d5e6378b6b9
damsara10 <damsaranawarathnasgn@gmail.com>
2025-03-22 02:02:31 +0530

    Delete SEU_IS_2022_ICT_060/03.06 directory

--- SEU_IS_2022_ICT_060/03.06/Task01.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main () {
-    int studentID=12345;
-    float GPA=3.75;
-    char grade='A';
-
-    printf("studentID:%d,\nGPA:%.2f,\nGrade:%c",studentID,GPA,grade);
-    return 0;
-}
--- SEU_IS_2022_ICT_060/03.06/Task08.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    int value =6;
-    char result = (value % 2 == 0) ? 'E' : '0';
-    printf("value is %c (Even, 0: odd)\n", result);
-
-    return 0;
-}
